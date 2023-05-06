dd5f1fd4703222a11e7e5bbb300793780780b96c
serbatinho <lserbato@gmail.com>
2023-05-06 11:13:57 -0300

    JS até inicio de array

---
+++ src/js/atribuicaoVariavel.js
@@ -0,0 +1,11 @@
+const idade = 22;
+let nome = "Lucas";
+const sobrenome = "Serbato";
+
+console.log(nome, sobrenome);
+console.log(`Meu nome é ${nome}`);
+
+nome = nome + sobrenome;
+console.log(nome)
+nome = 2;
+console.log(nome)
