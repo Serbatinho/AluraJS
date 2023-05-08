cbc19a9d8183384bfe0b5a677c09b7384a967bfc
serbatinho <lserbato@gmail.com>
2023-05-08 05:43:53 -0300

    Inicio Classes Abstratas

--- src/js/ContaCorrente.js
+++ src/js/ContaCorrente.js
@@ -2,18 +2,14 @@ import { Conta } from "./Conta.js";
 
 export class ContaCorrente extends Conta{
     static numeroDeContas = 0;
- 
-    constructor(cliente,agencia){
-        super(0,cliente,agencia);
+    constructor(cliente, agencia) {
+        super(0,cliente, agencia);
         ContaCorrente.numeroDeContas += 1;
     }
 
-    sacar(valor){
+    //sobreescrevendo o comportamento de sacar
+    sacar(valor) {
         let taxa = 1.1;
-        const valorSacado = taxa * valor;
-        if(this._saldo >= valorSacado){
-            this._saldo -= valorSacado;
-            return valorSacado;
-        }
+        return this._sacar(valor, taxa);
     }
 }
